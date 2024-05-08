a6526dd1f8c282b2734b552eabb39ee3115ccf0c
ShupeiLi <shupei.tina.li@gmail.com>
2024-05-08 16:34:33 +0200

    feat: add bash scripts

--- scripts/redial-conceptnet.sh
+++ scripts/redial-conceptnet.sh
@@ -1,32 +1,27 @@
-#! /usr/bin
-# Change n, key
+#! /usr/bash
+# Change n
 
 nhop="2"
 nkey="two"
 model="c2crs"
-save_path="/root/autodl-fs/${model}/tgredial-cndbpedia"
+save_path="/root/autodl-fs/${model}/redial-conceptnet"
 
-file1="entity2id.json"
-file2="cn-dbpedia.txt"
-file3="movie_ids.json"
-file1_alias="cndbpedia-${nkey}-hop-kg-entity2id.json"
-file2_alias="cndbpedia-${nkey}-hop-kg.txt"
-file3_alias="${nkey}-hop-movie_ids.json"
+file1="conceptnet_subkg.txt"
+file2="concept2id.json"
+file1_alias="conceptnet_${nkey}_hop_edges.txt"
+file2_alias="conceptnet_${nkey}_hop_key2index.json"
 
-data_path="$(pwd)/data/dataset/tgredial/pkuseg"
-kg_path="$(pwd)/ali-kg/cndbpedia/${nhop}-hop-c2crs"
+data_path="$(pwd)/data/dataset/redial/nltk"
+kg_path="$(pwd)/ali-kg/conceptnet-redial/${nhop}-hop"
 tmp_path="$(pwd)/data"
 
-# move and rename n-hop files
+# move n-hop files
 mv "${data_path}/${file1}" "${tmp_path}"
 mv "${data_path}/${file2}" "${tmp_path}"
-mv "${data_path}/${file3}" "${tmp_path}"
 cp "${kg_path}/${file1_alias}" "${data_path}"
 mv "${data_path}/${file1_alias}" "${data_path}/${file1}"
 cp "${kg_path}/${file2_alias}" "${data_path}"
 mv "${data_path}/${file2_alias}" "${data_path}/${file2}"
-cp "${kg_path}/${file3_alias}" "${data_path}"
-mv "${data_path}/${file3_alias}" "${data_path}/${file3}"
 
 # create backup folders
 mkdir -p log-backup/rec-train
@@ -38,11 +33,11 @@ mkdir -p model-backup
 # run model
 # recommendation
 # train
-. scripts/tgredial/train/tgredial_rec_train.sh
+. scripts/redial/train/redial_rec_train.sh
 mv log/* log-backup/rec-train
 mv "save/$(ls save)" "save/rec-train"
 # evaluation
-. scripts/tgredial/eval/tgredial_rec_eval.sh
+. scripts/redial/eval/redial_rec_eval.sh
 mv log/* log-backup/rec-eval
 mv save/rec-train model-backup
 rm -r "save/$(ls save)"
@@ -50,26 +45,24 @@ mv model-backup/rec-train save
 
 # conversation
 # train
-. scripts/tgredial/train/tgredial_conv_train.sh
+. scripts/redial/train/redial_conv_train.sh
 mv log/* log-backup/conv-train
 mv save/rec-train model-backup
 mv "save/$(ls save)" "save/conv-train"
 # evaluation
-. scripts/tgredial/eval/tgredial_conv_eval.sh
+. scripts/redial/eval/redial_conv_eval.sh
 mv log/* log-backup/conv-eval
 mv save/conv-train model-backup
 
 # backup
 mkdir -p "${save_path}/${nhop}-hop/"
 cp -R log-backup "${save_path}/${nhop}-hop/"
-zip -r "tgredial-cndbpedia-${nhop}hop-model.zip" model-backup
-mv "tgredial-cndbpedia-${nhop}hop-model.zip" "${save_path}/${nhop}-hop/"
+zip -r "redial-conceptnet-${nhop}hop-model.zip" model-backup
+mv "redial-conceptnet-${nhop}hop-model.zip" "${save_path}/${nhop}-hop/"
 
 # clean up
 sh scripts/clean.sh
 rm "${data_path}/${file1}"
 rm "${data_path}/${file2}"
-rm "${data_path}/${file3}"
 mv "${tmp_path}/${file1}" "${data_path}"
 mv "${tmp_path}/${file2}" "${data_path}"
-mv "${tmp_path}/${file3}" "${data_path}"
--- scripts/tgredial-hownet.sh
+++ scripts/tgredial-hownet.sh
@@ -4,29 +4,24 @@
 nhop="2"
 nkey="two"
 model="c2crs"
-save_path="/root/autodl-fs/${model}/tgredial-cndbpedia"
+save_path="/root/autodl-fs/${model}/tgredial-hownet"
 
-file1="entity2id.json"
-file2="cn-dbpedia.txt"
-file3="movie_ids.json"
-file1_alias="cndbpedia-${nkey}-hop-kg-entity2id.json"
-file2_alias="cndbpedia-${nkey}-hop-kg.txt"
-file3_alias="${nkey}-hop-movie_ids.json"
+file1="hownet.txt"
+file2="word2id.json"
+file1_alias="hownet-${nkey}-hop-kg.txt"
+file2_alias="hownet-${nkey}-hop-word2id.json"
 
 data_path="$(pwd)/data/dataset/tgredial/pkuseg"
-kg_path="$(pwd)/ali-kg/cndbpedia/${nhop}-hop-c2crs"
+kg_path="$(pwd)/ali-kg/hownet/${nhop}-hop"
 tmp_path="$(pwd)/data"
 
 # move and rename n-hop files
 mv "${data_path}/${file1}" "${tmp_path}"
 mv "${data_path}/${file2}" "${tmp_path}"
-mv "${data_path}/${file3}" "${tmp_path}"
 cp "${kg_path}/${file1_alias}" "${data_path}"
 mv "${data_path}/${file1_alias}" "${data_path}/${file1}"
 cp "${kg_path}/${file2_alias}" "${data_path}"
 mv "${data_path}/${file2_alias}" "${data_path}/${file2}"
-cp "${kg_path}/${file3_alias}" "${data_path}"
-mv "${data_path}/${file3_alias}" "${data_path}/${file3}"
 
 # create backup folders
 mkdir -p log-backup/rec-train
@@ -62,14 +57,12 @@ mv save/conv-train model-backup
 # backup
 mkdir -p "${save_path}/${nhop}-hop/"
 cp -R log-backup "${save_path}/${nhop}-hop/"
-zip -r "tgredial-cndbpedia-${nhop}hop-model.zip" model-backup
-mv "tgredial-cndbpedia-${nhop}hop-model.zip" "${save_path}/${nhop}-hop/"
+zip -r "tgredial-hownet-${nhop}hop-model.zip" model-backup
+mv "tgredial-hownet-${nhop}hop-model.zip" "${save_path}/${nhop}-hop/"
 
 # clean up
 sh scripts/clean.sh
 rm "${data_path}/${file1}"
 rm "${data_path}/${file2}"
-rm "${data_path}/${file3}"
 mv "${tmp_path}/${file1}" "${data_path}"
 mv "${tmp_path}/${file2}" "${data_path}"
-mv "${tmp_path}/${file3}" "${data_path}"
